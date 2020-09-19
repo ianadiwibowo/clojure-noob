a7da68c13a7dba7b9e912cbe98d5ba16fbd915e7
Ian Adiwibowo <ian.adiwibowo@gmail.com>
2020-09-19 15:55:47 +0700

    Try forms and operations

--- src/clojure_noob/core.clj
+++ src/clojure_noob/core.clj
@@ -4,4 +4,13 @@
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
+
+  ; OPERATIONS
+  ; This is how Clojure does operations, yeah weirdo
+  ; (operator operand1 operand2 ... operandn)
+  (+ 1 2 3) ; => 6
+  (/ 60 12) ; => 5
+  (- 33 3) ; => 30
+  (* 4 5 5) ; => 100
+  (str "Pupuru" "neko") ; => "Pupuruneko" (concatenates string)
+)
