82238ddd65a913881249d747baffe3be02aaff04
Ian Adiwibowo <ian.adiwibowo@gmail.com>
2020-09-19 16:40:07 +0700

    Try if, when, do, and nil? operators

--- src/clojure_noob/core.clj
+++ src/clojure_noob/core.clj
@@ -5,6 +5,9 @@
   "I don't do a whole lot ... yet."
   [& args]
 
+  ; DEBUGGING
+  (println "Hello Pupuruneko!")
+
   ; OPERATIONS
   ; This is how Clojure does operations, yeah weirdo
   ; (operator operand1 operand2 ... operandn)
@@ -13,4 +16,38 @@
   (- 33 3) ; => 30
   (* 4 5 5) ; => 100
   (str "Pupuru" "neko") ; => "Pupuruneko" (concatenates string)
-)
+
+  ; IF
+  ; (if condition-form
+  ;   then-form
+  ;   optional-else-form)
+  (if true
+    "Pupuruneko TRUE"
+    "Pupuruneko FALSE") ; => "Pupuruneko TRUE"
+
+  (if true
+    "Pupuruneko TRUE") ; => "Pupuruneko TRUE"
+
+  (if false
+    "Pupuruneko TRUE"
+    "Pupuruneko FALSE") ; => "Pupuruneko FALSE"
+
+  ; `do` for multiple expressions
+  (if  true
+    (do
+      (println "Success!")
+      "Pupuruneko TRUE")
+    (do
+      (println "Failed!")
+      "Pupuruneko FALSE"))
+
+  ; WHEN
+  ; Just like `if` but with `do`
+  (when true
+    (println "Success!")
+    "Pupuruneko TRUE") ; => "Pupuruneko TRUE"
+
+  ; NIL
+  (nil? 7) ; => false
+  (nil? nil) ; => true
+  )
